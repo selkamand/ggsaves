838dec32e037822ce4f3adcd931e3ea5f6642eab
selkamand <sam.elkamand@gmail.com>
2024-02-08 00:21:53 +1100

    fix: cleaned up docs and addd ggisaves

--- R/ggsaves.R
+++ R/ggsaves.R
@@ -25,3 +25,52 @@ ggsaves <- function(plot, prefix, outfolder, dpi = 300, width = 8, height = 5, .
   invisible(NULL)
 
 }
+
+
+#' Save interactive plot to common filetypes
+#'
+#' @inheritParams htmlwidgets::saveWidget
+#' @param width,height Plot size in units ("in", "cm", "mm", or "px"). If not supplied, uses the size of current graphics device.
+#' @param prefix filename without extension
+#' @param outfolder folder to save files in
+#'
+#' @return Run for its side effects. Returns invisible(NULL)
+#' @export
+#'
+#' @examples
+#'
+#' \dontrun{
+#'  library(ggiraph)
+#'  library(ggplot2)
+#'
+#'  gg_plot <- iris |>
+#'      ggplot(aes(Sepal.Length, Sepal.Width)) +
+#'      geom_point_interactive(aes(data_id  = Species, tooltip = Species))
+#'
+#'    interactive_gg_plot <- girafe(ggobj = gg_plot)
+#'
+#'    ggisaves(interactive_gg_plot, prefix = "my_interactive_plot", outfolder = ".")
+#' }
+ggisaves <- function(plot, prefix, outfolder, dpi = 300, title = prefix, knitrOptions, background = "white", width = 8, height = 5, ...){
+
+  # Outfiles
+  extensionless_filepath <- paste0(outfolder, "/", prefix)
+  outfile_html <- paste0(extensionless_filepath, '.html')
+  outfile_svg <- paste0(extensionless_filepath, '.svg')
+  outfile_pdf <- paste0(extensionless_filepath, '.pdf')
+
+
+  # [Export HTLM, SVG, and Vector PDF]
+
+  # HTML
+  htmlwidgets::saveWidget(widget = plot, file = outfile_html, selfcontained = TRUE, title = title, knitrOptions = knitrOptions, background = background)
+
+  # SVG
+  writeLines(plot$x$html, outfile_svg)
+
+  # Vector PDF
+  rsvg::rsvg_pdf(svg = outfile_svg, file = outfile_pdf)
+
+  invisible(NULL)
+
+}
--- README.md
+++ README.md
@@ -6,14 +6,30 @@
 <!-- badges: start -->
 
 [![R-CMD-check](https://github.com/selkamand/ggsaves/actions/workflows/R-CMD-check.yaml/badge.svg)](https://github.com/selkamand/ggsaves/actions/workflows/R-CMD-check.yaml)
+[![CRAN
+status](https://www.r-pkg.org/badges/version/ggsaves)](https://CRAN.R-project.org/package=ggsaves)
 <!-- badges: end -->
 
-The goal of ggsaves is to allow you to save PDF, 300dpi PNG, and SVG
-versions of the same plot all at once
+ggsaves saves a ggplot as a PDF, 300dpi PNG, and SVG all at once. This
+is an exceptionally simple convenience package that wraps
+`ggplot2::ggsave()`
 
 ## Installation
 
-You can install the development version of ggsaves from
+Install ggsaves from CRAN
+
+``` r
+install.packages('ggsaves')
+#> Installing package into '/private/var/folders/93/mb2bzm5j2v7625bbl3pnxb1h0000gp/T/RtmpxMQZ5K/temp_libpath48ac31971f15'
+#> (as 'lib' is unspecified)
+#> Warning: package 'ggsaves' is not available for this version of R
+#> 
+#> A version of this package for your version of R might be available elsewhere,
+#> see the ideas at
+#> https://cran.r-project.org/doc/manuals/r-patched/R-admin.html#Installing-packages
+```
+
+Alternatively, install the development version of ggsaves from
 [GitHub](https://github.com/) with:
 
 ``` r
@@ -31,5 +47,5 @@ my_gg_plot <- mtcars |>
   ggplot(aes(mpg, cyl)) + 
   geom_point() 
 
-ggsaves(my_gg_plot, prefix = "asdsa",  outfolder = ".")
+ggsaves(my_gg_plot, prefix = "myplot",  outfolder = ".")
 ```
